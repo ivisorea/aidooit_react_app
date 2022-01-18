feb56ee270aa175149e19f9accec7af90568eb00
carina <c.malgut@googlemail.com>
2022-01-18 17:52:53 +0100

    Input

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,9 +1,11 @@
+
+
 import React from 'react'
 import { Navigate, useLocation} from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import { useAuth } from '../../context/AuthContext';
 import './Login.css'
-import { Button, TextBtn } from '../Toolbar/styles';
+import { Button, TextBtn} from '../Toolbar/styles';
  
  export const Login = () => {
    const {
@@ -20,57 +22,59 @@ import { Button, TextBtn } from '../Toolbar/styles';
   if (isAuthenticated) return <Navigate to={from} replace />;
  
  
-   return (
-     <div className="">
-       <div className="login-box">
-         <div className="">
-           <div className="col-8 "></div>
-           <form onSubmit={handleSubmit(onSubmit)} className="input-field">
-             
+  return (
+    <div className="login-box" style={{ height: '30rem' , width: '25rem', paddingTop:'4rem', borderRadius:'20px'}}>
+      <div className="col-8 "></div>
+        <form onSubmit= {handleSubmit(onSubmit)}>
+          <div class="form-floating mb-3">
              <input
                type="email"
                className={
                  errors.name ? "form-control is invalid" : "form-control"
                }
-               style={{ marginTop: '4rem' }}
+               style={{ marginBottom: '1rem' }}
                placeholder="Email"
                {...register("email", { required: true })}
              />
+             <label for="floatingInput">Email</label>
              {errors.email && <div>Email is required</div>}
- 
-             <br />
+          </div>
+            <br />
+          <div class="form-floating">
              <input
                type="password"
                className={
                  errors.name ? "form-control is invalid" : "form-control"
                }
-               style={{ marginBottom: '4rem' }}
+               style={{ marginBottom: '1rem' }}
                placeholder="Password"
                {...register("password", { required: true })}
              />
+             <label for="floatingInput">Password</label>
              {errors.password && <div>Password is required</div>}
-             
+             </div>
+            <br/>
            
-             <Button onClick={Login} className="btn rounded-pill login-button"  >
-                <TextBtn>LOGIN</TextBtn>
-             </Button>
-              
-           </form>
- 
-           <div className="login-text">
-           </div>
-
-         </div>
+            <Button onClick={Login} className="login-button" className="btn rounded-pill login-button"> 
+            <TextBtn>LOGIN</TextBtn> 
+            </Button>
+            
+            </form>
+          <div className="login-text">
+            <h5>Don`t have an accountAlread? Sign Up</h5>
+                  
+           
+          
+            </div> 
+            
        </div>
-       <br/>
-       <h5>Don't have an account? Sign Up</h5>
-     </div>
+      
+      
 
 
-
-
-
-   );
+);
  };
- 
- export default Login;
+
+
+export default Login;
+
--- src/components/Register/index.js
+++ src/components/Register/index.js
@@ -4,6 +4,7 @@ import { Navigate, useLocation } from 'react-router-dom';
 import "./Register.css";
 import { useForm } from 'react-hook-form';
 import { useAuth } from '../../context/AuthContext';
+import { Button, TextBtn } from '../Toolbar/styles';
 
 
 export const Register = () => {
@@ -19,63 +20,88 @@ const {isAuthenticated, signup} = useAuth ()
  if (isAuthenticated) return <Navigate to={from} replace />;
 
   return (
-    <div className="login">
-      <div className="login-box">
-        <div className="login-container">
-          <div className="col-8"></div>
-          <form onSubmit={handleSubmit(onSubmit)} className="input-field">
-            <input
-              type="text"
-              className={
-                errors.name ? "form-control is invalid" : "form-control"
-              }
-              placeholder="First name"
-              {...register("first_name", { required: true })}
-            />
-            {errors.first_name && <div>First name is required</div>}
+    <div className="login-box" style={{ height: '30rem' , width: '25rem', paddingTop:'4rem', borderRadius:'20px'}}>
+        
+    <div className="col-8 "></div>
+    <div class="form-floating mb-3">
+             <input
+               type="text"
+               className={
+                 errors.name ? "form-control is invalid" : "form-control"
+               }
+               style={{ marginBottom: '1rem' }}
+               placeholder="First name"
+               {...register("first_name", { required: true })}
+             />
+             <label for="floatingInput">First name</label>
+             {errors.email && <div>First name is required</div>}
+ 
             <br />
-            <input
-              type="text"
-              className={
-                errors.name ? "form-control is invalid" : "form-control"
-              }
-              placeholder="Last name"
-              {...register("last_name", { required: true })}
-            />
-            {errors.last_name && <div>Last name is required</div>}
-            <br />
-            <input
-              type="email"
-              className={
-                errors.name ? "form-control is invalid" : "form-control"
-              }
-              placeholder="Email"
-              {...register("email", { required: true })}
-            />
-            {errors.email && <div>Email is required</div>}
+
+
+             <div class="form-floating mb-3">
+             <input
+               type="text"
+               className={
+                 errors.name ? "form-control is invalid" : "form-control"
+               }
+               style={{ marginBottom: '1rem' }}
+               placeholder="Last name"
+               {...register("last_name", { required: true })}
+             />
+             <label for="floatingInput">Last name</label>
+             {errors.email && <div>Last name is required</div>}
 
             <br />
-            <input
-              type="password"
-              className={
-                errors.name ? "form-control is invalid" : "form-control"
-              }
-              placeholder="Password"
-              {...register("password", { required: true })}
-            />
-            {errors.password && <div>Password is required</div>}
+
+          <div class="form-floating mb-3">
+             <input
+               type="email"
+               className={
+                 errors.name ? "form-control is invalid" : "form-control"
+               }
+               style={{ marginBottom: '1rem' }}
+               placeholder="Email"
+               {...register("email", { required: true })}
+             />
+             <label for="floatingInput">Email</label>
+             {errors.email && <div>Email is required</div>}
+ 
+
+            <br />
+            <div class="form-floating">
+             <input
+               type="password"
+               className={
+                 errors.name ? "form-control is invalid" : "form-control"
+               }
+               style={{ marginBottom: '1rem' }}
+               placeholder="Password"
+               {...register("password", { required: true })}
+             />
+             <label for="floatingInput">Password</label>
+             {errors.password && <div>Password is required</div>}
             <br/>
            
-           <button onClick={Register} className="login-button"  >Sign Up</button>
-          </form>
+            <Button onClick={Register} className="login-button" className="btn rounded-pill login-button"> 
+            <TextBtn>SIGN UP</TextBtn> 
+            </Button>
+         
 
           <div className="login-text">
             <h5>Already have an account? Log In</h5>
+           </div>
           </div>
+         </div>
+         </div>
         </div>
-      </div>
-    </div>
-  );
+       </div>
+
+
+
+);
 };
 
+
 export default Register;
+
