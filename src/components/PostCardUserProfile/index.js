b6c2fb43b3123ad4cada38503dbb958d97978486
carina <c.malgut@googlemail.com>
2022-01-20 10:18:14 +0100

    delete

--- src/components/PostCardUserProfile/index.js
+++ src/components/PostCardUserProfile/index.js
@@ -3,14 +3,32 @@ import ShowMoreText from "react-show-more-text";
 import { Img, PostWrapper, DescWrapper, Title, Button, Card, BtnContainer } from './styles';
 import { Parser } from 'html-to-react'
 import {TextBtn } from '../Toolbar/styles';
+import axios from "axios";
+
+
 
 
 export const PostCardUserProfile = ({_id, image, title, body, likes}) => {
+
+  
+
+    function handleDelete () {
+       
+        axios.delete((`https://aidooit-app.herokuapp.com/post/author/${_id}`)
+        .then((res) => {console.log(res);
+           
+            
+        }))
+      }
 
 
 
 
     return (
+
+
+
+
         <Card>
             <a href={`/detail/${_id}`}>
                 <PostWrapper>
@@ -42,17 +60,31 @@ export const PostCardUserProfile = ({_id, image, title, body, likes}) => {
                         </button>
                      </BtnContainer>
 
+
                     <BtnContainer>
-                        <button className="btn" /*onClick={deletePost}*/>
+                        <button className="btn" onClick={handleDelete}>
                         <TextBtn>DELETE</TextBtn> 
                         </button>
                     </BtnContainer> 
+
+
+
+
+
+
+                    
+
+
                  
                 </PostWrapper>
+
             </a>
 
 
         </Card>
+
+
+
     )
 }
 
